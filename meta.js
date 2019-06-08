f5b75f1dd41c0028e4dfd418a1043a0c249d75fe
Wei Gao <wgao19@gmail.com>
2019-06-08 13:01:03 +0000

    feat: use a meta file for meta data

---
+++ meta.js
@@ -0,0 +1,5 @@
+export default {
+  title: "title",
+  speaker: "Wei Gao",
+  twitter: "wgao19"
+};
--- theme.js
+++ theme.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Highlight, { defaultProps } from 'prism-react-renderer';
-import nightOwlTheme from 'prism-react-renderer/themes/nightOwl';
+import React from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import nightOwlTheme from "prism-react-renderer/themes/nightOwl";
+import meta from "./meta.js";
 
 const Code = ({ children, className: [languageClassName] }) => {
   const language =
-    languageClassName && languageClassName.replace(/language-/, '');
+    languageClassName && languageClassName.replace(/language-/, "");
   return (
     <Highlight
       {...defaultProps}
@@ -16,7 +17,7 @@ const Code = ({ children, className: [languageClassName] }) => {
         <pre
           className={className}
           style={style}
-          css={{ padding: '1rem', borderRadius: '4px' }}
+          css={{ padding: "1rem", borderRadius: "4px" }}
         >
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
@@ -24,7 +25,7 @@ const Code = ({ children, className: [languageClassName] }) => {
                 <span
                   key={key}
                   {...getTokenProps({ token, key })}
-                  css={{ fontSize: '.75em' }}
+                  css={{ fontSize: ".75em" }}
                 />
               ))}
             </div>
@@ -35,31 +36,31 @@ const Code = ({ children, className: [languageClassName] }) => {
   );
 };
 
-const Provider = ({ title, author, twitter, children }) => (
+const Provider = ({ title, speaker, twitter, children }) => (
   <React.Fragment>
     <nav
       css={{
-        padding: '1rem',
-        display: 'flex',
-        width: '100vw',
-        position: 'fixed',
+        padding: "1rem",
+        display: "flex",
+        width: "100vw",
+        position: "fixed",
         top: 0,
-        background: 'white',
+        background: "white",
         fontFamily:
           'Raleway, "Open Sans", ".SF NS Text", "Helvetica Neue", sans-serif',
-        fontSize: '1.5rem',
+        fontSize: "1.5rem"
       }}
     >
       <span
         css={{
-          color: '#222',
+          color: "#222"
         }}
       >
-        {author}
+        {speaker}
       </span>
-      <span css={{ padding: '0 .25em' }}>—</span>
-      <span css={{ color: '#aaa' }}>{title}</span>
-      <span css={{ marginLeft: 'auto', color: '#aaa' }}>June 7, 2019</span>
+      <span css={{ padding: "0 .25em" }}>—</span>
+      <span css={{ color: "#aaa" }}>{title}</span>
+      <span css={{ marginLeft: "auto", color: "#aaa" }}>June 7, 2019</span>
     </nav>
     <main>{children}</main>
   </React.Fragment>
@@ -67,52 +68,52 @@ const Provider = ({ title, author, twitter, children }) => (
 
 const custom = {
   css: {
-    fontSize: '18px',
+    fontSize: "18px"
   },
   font: 'Raleway, "Open Sans", sans-serif',
   components: {
-    code: Code,
+    code: Code
   },
-  monospace: 'Dank Mono',
+  monospace: "Dank Mono",
   googleFont:
-    'https://fonts.googleapis.com/css?family=Raleway:400,400i|Zilla+Slab:700&display=swap',
+    "https://fonts.googleapis.com/css?family=Raleway:400,400i|Zilla+Slab:700&display=swap",
   h1: {
-    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif',
+    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif'
   },
   h2: {
-    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif',
+    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif'
   },
   h3: {
-    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif',
+    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif'
   },
   h4: {
-    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif',
+    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif'
   },
   h5: {
-    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif',
+    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif'
   },
   h6: {
-    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif',
+    fontFamily: '"Zilla Slab", ".SF NS Display", sans-serif'
   },
   blockquote: {
-    width: '50vw',
-    left: '-15vw',
-    width: '50vw',
-    position: 'relative',
-    textAlign: 'left',
+    width: "50vw",
+    left: "-15vw",
+    width: "50vw",
+    position: "relative",
+    textAlign: "left",
     fontWeight: 700,
-    fontFamily: 'Zilla Slab',
-    lineHeight: 1,
+    fontFamily: "Zilla Slab",
+    lineHeight: 1
   },
   /**
    * Looks like this is the only way to pass down a component to wrap around everything
    * Semantically should not write meta data here
    */
   Provider: ({ children }) => (
-    <Provider author="Wei Gao" title="Title" twitter="wgao19">
+    <Provider speaker={meta.speaker} title={meta.title} twitter={meta.twitter}>
       {children}
     </Provider>
-  ),
+  )
 };
 
 export default custom;
