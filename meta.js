5065680383771772cc6857a5f0d09c85158824e2
Wei Gao <wgao19@gmail.com>
2019-06-09 16:37:26 +0000

    chore: add more default meta

--- meta.js
+++ meta.js
@@ -2,5 +2,7 @@ export default {
   title: "title",
   speaker: "Wei Gao",
   twitter: "wgao19",
-  date: "June 7, 2019"
+  date: "June 7, 2019",
+  event: "React Knowledgeable",
+  eventLink: "https://twitter.com/reknowledgeable"
 };
